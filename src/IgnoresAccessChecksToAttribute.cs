6aef6692f81942789d0d89b274f40f86c1b99d16
Jake Rich <weirdo44@hotmail.com>
2020-02-23 15:39:34 -0500

    Avoid exposing public fields

---
+++ src/IgnoresAccessChecksToAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+using System.Text;
+
+[assembly: SecurityPermission( SecurityAction.RequestMinimum, SkipVerification = true )]
+[assembly: IgnoresAccessChecksTo( "Assembly-CSharp" )]
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage( AttributeTargets.Assembly, AllowMultiple = true )]
+    public class IgnoresAccessChecksToAttribute : Attribute
+    {
+        public IgnoresAccessChecksToAttribute( string assemblyName )
+        {
+            this.AssemblyName = assemblyName;
+        }
+
+        public string AssemblyName { get; }
+    }
+}
